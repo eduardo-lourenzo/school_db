f7a1df5a541db3a9b56b835034095d5921032660
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-06 16:20:59 -0300

    build(SchoolDB): create StudentEntity class

---
+++ src/main/java/br/edu/zup/school_db/StudentEntity.java
@@ -0,0 +1,49 @@
+package br.edu.zup.school_db;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Range(min = 5, max = 150)
+    private Integer age;
+
+    public StudentEntity() {}
+
+    public StudentEntity(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
